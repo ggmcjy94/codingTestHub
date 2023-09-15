7bb6bd1bcd6fbb4cf7b4e445db8dd4ddbac4c826
Lee Hyun SEOK <54434182+ggmcjy94@users.noreply.github.com>
2023-09-15 12:42:14 +0900

    [Silver III] Title: 등수 매기기, Time: 860 ms, Memory: 60144 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i =0; i< N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        long sum = 0;
+        for (int i =0; i<N; i++) {
+            sum += Math.abs(arr[i] - (i+1));
+        }
+        bw.write(sum+"");
+        bw.flush();
+        bw.close();
+    }
+}
