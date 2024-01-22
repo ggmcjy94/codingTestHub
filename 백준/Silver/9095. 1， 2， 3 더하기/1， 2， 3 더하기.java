6f964e274b732e4b35c8c48d7270bcdac7110bc4
Lee Hyun SEOK <54434182+ggmcjy94@users.noreply.github.com>
2024-01-23 02:18:40 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 144 ms, Memory: 15752 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,28 +1,35 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int N,M,R,K,min,max;
+    static long ans;
+    static int dx[] = {0, 0, -1, -1, -1, 1, 1, 1};
+    static int dy[] = {1, -1, -1, 0, 1, -1, 0, 1};
+    static Queue<int[]> queue = new LinkedList<>();
+    static boolean[][] visited;
+    static StringBuilder sb = new StringBuilder();
 
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw  = new BufferedWriter(new OutputStreamWriter(System.out));
-
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
         int[] dp = new int[11];
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
-        for (int i = 4; i < 11; i++) {
-            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
-        }
 
-        int N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++){
-            int T = Integer.parseInt(br.readLine());
-            bw.write(dp[T] + "\n");
+        for (int i = 4; i < 11; i++) {
+            dp[i] = dp[i-3] +dp[i-2] + dp[i-1];
+        }
+        for (int i = 0; i< N; i++) {
+            bw.write(dp[Integer.parseInt(br.readLine())] +"\n");
         }
         bw.flush();
         bw.close();
+        br.close();
     }
 }
