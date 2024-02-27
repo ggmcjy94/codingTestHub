3b76eea1fd97833f987168f2ad072e73f3ad46f6
Lee Hyun SEOK <54434182+ggmcjy94@users.noreply.github.com>
2024-02-27 19:19:51 +0900

    [Silver II] Title: DFS와 BFS, Time: 232 ms, Memory: 19272 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,80 +1,84 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
+    static int T,N,C,V,S,E,M,Q,K,min,max;
+    static long ans;
+    static int[][] dis;
+    static int[] arr,nums,operator,order;
+    static int[] selected,used;
+    static int[] dp,A;
+    static int dx[] = {0, -1, 0 ,1};
+    static int dy[] = {1, 0, -1, 0};
 
-	static StringBuilder sb = new StringBuilder();
-	static boolean[] check;
-	static int[][] arr;
-	
-	static int node, line, start;
-	
-	static Queue<Integer> q = new LinkedList<>();
+    static ArrayList<ArrayList<Integer>> graph;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		node = Integer.parseInt(st.nextToken());
-		line = Integer.parseInt(st.nextToken());
-		start= Integer.parseInt(st.nextToken());
-		
-		arr = new int[node+1][node+1];
-		check = new boolean[node+1];
-		
-		for(int i = 0 ; i < line ; i ++) {
-			StringTokenizer str = new StringTokenizer(br.readLine());
-			
-			int a = Integer.parseInt(str.nextToken());
-			int b = Integer.parseInt(str.nextToken());
-			
-			arr[a][b] = arr[b][a] =  1;	
-		}
-			//sb.append("\n");
-			dfs(start);
-			sb.append("\n");
-			check = new boolean[node+1];
-			
-			bfs(start);
-			
-			System.out.println(sb);
-		
-		}
-	public static void dfs(int start) {
-		
-		check[start] = true;
-		sb.append(start + " ");
-		
-		for(int i = 0 ; i <= node ; i++) {
-			if(arr[start][i] == 1 && !check[i])
-				dfs(i);
-		}
-		
-	}
-	
-	public static void bfs(int start) {
-		q.add(start);
-		check[start] = true;
-		
-		while(!q.isEmpty()) {
-			
-			start = q.poll();
-			sb.append(start + " ");
-			
-			for(int i = 1 ; i <= node ; i++) {
-				if(arr[start][i] == 1 && !check[i]) {
-					q.add(i);
-					check[i] = true;
-				}
-			}
-		}
-		
-		
-	}
+
+    static void bfs(int k) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(k);
+        visited[k] = true;
+
+        while (!queue.isEmpty()) {
+            Integer poll = queue.poll();
+            sb.append(poll).append(" ");
+            for (Integer i : graph.get(poll)) {
+                if (!visited[i]) {
+                    queue.add(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+
+    static void dfs (int k) {
+        visited[k] = true;
+        sb.append(k).append(" ");
+        for (Integer i : graph.get(k)) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+        for (int i = 1; i <=N; i++) {
+            Collections.sort(graph.get(i));
+        }
+
+        visited = new boolean[N+1];
+        dfs(V);
+        sb.append('\n');
+        visited = new boolean[N+1];
+        bfs(V);
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 
 }
