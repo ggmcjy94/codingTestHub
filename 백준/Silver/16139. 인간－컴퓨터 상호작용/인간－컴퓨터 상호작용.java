88e88d18cc58b42b8d18e06b902b7725650e7d0d
Lee Hyun SEOK <54434182+ggmcjy94@users.noreply.github.com>
2024-03-22 16:44:53 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 920 ms, Memory: 116812 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -16,59 +16,27 @@ public class Main {
     static boolean[] visit;
     static int[] dist;
     static StringBuilder sb = new StringBuilder();
-    static void full() {
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-                map[i][j] = 'O';
-            }
-        }
-    }
-
-    static void boom(){
-        int[] dx = new int[]{1,0,-1,0};
-        int[] dy = new int[]{0,1,0,-1};
-        boolean[][] visited = new boolean[R][C];
-        int cnt = 0;
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-                if (map[i][j] == 'O') {
-                    cnt++;
-                    visited[i][j] = true;
-                    for (int k = 0; k < 4; k++) {
-                        int nex = i + dx[k];
-                        int ney = j + dy[k];
-                        if (nex < 0 || ney <0 || nex >= R || ney >= C) continue;
-                        if (visited[nex][ney]) continue;
-                        visited[nex][ney] = true;
-                    }
-                }
-            }
-        }
-        if(cnt > 0) {
-            for (int i = 0; i < R; i++) {
-                for (int j = 0; j<C; j++) {
-                    if (visited[i][j]) map[i][j] = '.';
-                    else map[i][j] = 'O';
-                }
-            }
-        }
-
-    }
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String val = br.readLine();
+
+        arr=new int[val.length()+1][26];
+        for (int i = 1; i <= val.length(); i++) {
+            for (int j = 0; j < 26; j++) {
+                arr[i][j] = arr[i-1][j];
+            }
+            arr[i][val.charAt(i-1) - 'a'] += 1;
+        }
         N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++) {
+
+        while (N-- > 0) {
             st = new StringTokenizer(br.readLine());
-            String target = st.nextToken();
+            int tar = st.nextToken().charAt(0) -'a';
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            int cnt = 0;
-            for (int j = start; j <= end; j++) {
-                if (val.charAt(j) == target.charAt(0)) cnt++;
-            }
-            bw.write(cnt+"\n");
+            bw.write(arr[end+1][tar] - arr[start][tar] +"\n");
         }
 
         bw.flush();
