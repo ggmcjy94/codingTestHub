da3fc1464df09a84af527fc1d339a7b4fa08f1b7
Lee Hyun SEOK <54434182+ggmcjy94@users.noreply.github.com>
2024-03-28 00:22:47 +0900

    [Bronze V] Title: 꼬마 정민, Time: 228 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.*;
+
+public class Main {
+
+    static int N,Q,M,C,R,T,A,B;
+    static int ans;
+    static StringTokenizer st;
+    static int[] dx = {1,0,-1,0,1,-1,-1,1};
+    static int[] dy = {0,1,0,-1,1,1,-1,-1};
+    static int[] arr, brr;
+    static ArrayList<Integer>[] graph;
+    static char[][] map;
+    static Queue<int[]> iceQueue;
+    static boolean[][] visited;
+    static boolean[] visit;
+    static int[] dist;
+    static StringBuilder sb = new StringBuilder();
+
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        bw.write(a+b+c+"");
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+}
+
+
