d8a0fc4811593894960f8b21180cb92b8e0e9f99
Lee Hyun SEOK <54434182+ggmcjy94@users.noreply.github.com>
2024-12-12 16:37:05 +0900

    Title: 8 x 8 표 이동, Time: 88ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241212/8 x 8 표 이동/move-8-x-8-table.java
+++ 241212/8 x 8 표 이동/move-8-x-8-table.java
@@ -8,36 +8,35 @@ public class Main {
     static int[][] arr;
     static int[] dx = {-1, 1, 0, 0, -1, -1, 1, 1};
     static int[] dy = {0, 0, -1, 1, -1, 1, -1, 1};
-    static List<int[]> wallList = new ArrayList<>();
-    static boolean[][] meetList;
-    static boolean[][] visited;
-    static boolean[] visit;
-    static Queue<int[]> queue = new LinkedList<>();
 
 
 
-    static int bfs(int x, int y) {
-        visited[x][y] = true;
-        queue.add(new int[]{x,y});
+    static int bfs() {
+        boolean[][] visited = new boolean[8][8];
+        Queue<int[]> queue = new LinkedList<>();
+
+        // 시작 위치 (7, 0) 추가
+        queue.offer(new int[]{7, 0});
+        visited[7][0] = true;
+
         while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] poll = queue.poll();
+                int nox = poll[0];
+                int noy = poll[1];
 
-            int[] poll = queue.poll();
-            int nox = poll[0];
-            int noy = poll[1];
-
-            if (map[nox][noy] == '#') continue;
-
-            if (nox == 0 && noy == 7) {
-                return 1;
-            }
-
-            for (int i = 0; i < 8; i++) {
-                int nex = nox + dx[i];
-                int ney = noy + dy[i];
-                if (nex < 0 || ney < 0 || nex>=N || ney >= M || visited[nex][ney]) continue;
-                if (map[nex][ney] == '.') {
-                    queue.offer(new int[]{nex, ney});
-                    visited[nex][ney] = true;
+                if (nox == 0 && noy == 7) {
+                    return 1;
+                }
+                if (map[nox][noy] == '#') continue;
+                for (int j = 0; j < 8; j++) {
+                    int nex = nox + dx[j];
+                    int ney = noy + dy[j];
+                    if (nex >= 0 && nex < 8 && ney >= 0 && ney < 8 && !visited[nex][ney] && map[nex][ney] == '.') {
+                        visited[nex][ney] = true;
+                        queue.offer(new int[]{nex, ney});
+                    }
                 }
             }
             moveWalls();
@@ -45,6 +44,7 @@ public class Main {
         return 0;
     }
     static void moveWalls() {
+
         for (int i = 7; i > 0; i--) {
             map[i] = Arrays.copyOf(map[i-1],8);
         }
@@ -60,14 +60,13 @@ public class Main {
         N = 8;
         M = 8;
         map = new char[N][M];
-        visited = new boolean[N][M];
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
             for (int j = 0; j < M; j++) {
                 map[i][j] = s.charAt(j);
             }
         }
-        bw.write(bfs(N-1, 0)+"");
+        bw.write(bfs()+"");
         bw.flush();
         bw.close();
         br.close();
