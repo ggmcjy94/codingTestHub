8b9f27bf0cabe0e68862751030ea33e4c01b2eba
Lee Hyun SEOK <54434182+ggmcjy94@users.noreply.github.com>
2024-12-23 14:15:28 +0900

    Title: 숫자 최대로 더하기, Time: 3064ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

---
+++ 241223/숫자 최대로 더하기/add-max-number.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N , M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        List<Double> arr = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            arr.add(Double.parseDouble(st.nextToken()));
+        }
+        Collections.sort(arr);
+        // 2 4 6 8
+        while (arr.size() != 1) {
+            double min = arr.get(0) / 2;
+            double max = arr.get(arr.size() - 1);
+            double sum = min + max;
+            arr.remove(0);
+            arr.remove(arr.size() - 1);
+            arr.add(sum);
+            Collections.sort(arr);
+        }
+        bw.write(Math.ceil(arr.get(0))+"");
+
+
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
