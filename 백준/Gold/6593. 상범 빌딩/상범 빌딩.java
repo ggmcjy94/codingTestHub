81cf73af4a9850351d2f95c74bad35d2d92d9fc7
Lee Hyun SEOK <54434182+ggmcjy94@users.noreply.github.com>
2025-02-25 15:22:02 +0900

    [Gold V] Title: 상범 빌딩, Time: 156 ms, Memory: 18400 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
@@ -6,31 +6,9 @@ public class Main {
     static int L,R ,C;
     static char[][][] map;
     static StringBuilder sb = new StringBuilder();
-    static int[] dx= {-1,1,0,0,0,0};
-    static int[] dy = {0,0,-1,1,0,0};
+    static int[] dx = {-1,1,0,0,0,0};
+    static int[] dy = {0,0,1,-1,0,0};
     static int[] dz = {0,0,0,0,1,-1};
-    //3 4 5
-    //S....
-    //.###.
-    //.##..
-    //###.#
-    //
-    //#####
-    //#####
-    //##.##
-    //##...
-    //
-    //#####
-    //#####
-    //#.###
-    //####E
-    //
-    //1 3 3
-    //S##
-    //#E#
-    //###
-    //
-    //0 0 0
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -45,24 +23,23 @@ public class Main {
                 bw.write(sb.toString());
                 break;
             }
-
             map = new char[L][R][C];
-
-            int sx=0,sy=0,sz=0;
-            for (int i = 0; i < L ; i++) {
+            int px = 0, py=0, pz=0;
+            for (int i = 0 ; i < L; i++) {
                 for (int j = 0; j < R; j++) {
                     String s = br.readLine();
                     for (int k = 0; k < C; k++) {
                         map[i][j][k] = s.charAt(k);
-                        if(map[i][j][k] == 'S') {
-                            sx =i; sy = j; sz =k;
+                        if (map[i][j][k] == 'S') {
+                            px=i;py=j;pz=k;
                             map[i][j][k] = '.';
                         }
                     }
                 }
                 br.readLine();
             }
-            bfs(sx, sy, sz);
+
+            bfs(px,py,pz);
         }
 
         bw.flush();
@@ -70,35 +47,35 @@ public class Main {
         br.close();
     }
 
-
-    static void bfs(int x , int y , int z) {
+    static void bfs(int x,int y, int z) {
         Queue<int[]> queue = new LinkedList<>();
-        boolean[][][] check = new boolean[L][R][C];
-        queue.add(new int[]{x, y, z, 0});
-        check[x][y][z] = true;
+        boolean[][][] visited = new boolean[L][R][C];
+        queue.add(new int[] {x, y, z , 0});
+        visited[x][y][z] = true;
 
         while (!queue.isEmpty()) {
             int[] poll = queue.poll();
-            int px = poll[0], py = poll[1] , pz = poll[2];
+            int px = poll[0];
+            int py = poll[1];
+            int pz = poll[2];
             int move = poll[3];
             if (map[px][py][pz] == 'E') {
-                sb.append("Escaped in " + move+" minute(s).\n");
+                sb.append("Escaped in ").append(move).append(" minute(s).\n");
                 return;
             }
-
             for (int i = 0; i < 6; i++) {
                 int nx = px + dx[i];
                 int ny = py + dy[i];
                 int nz = pz + dz[i];
-                if(nx <0 || ny <0 || nz<0 || nx > L-1 || ny>R-1 || nz > C-1) continue;
-                if (check[nx][ny][nz]) continue;
-                if (map[nx][ny][nz] =='.' || map[nx][ny][nz] == 'E') {
-                    check[nx][ny][nz] = true;
-                    queue.add(new int[]{nx,ny,nz, move+1});
+                if (nx < 0 || ny < 0 || nz < 0 || nx >= L || ny >= R || nz >= C) continue;
+                if (visited[nx][ny][nz]) continue;
+                if (map[nx][ny][nz] == '.' || map[nx][ny][nz] == 'E') {
+                    queue.add(new int[]{nx,ny,nz,move+1});
+                    visited[nx][ny][nz] = true;
                 }
             }
         }
         sb.append("Trapped!\n");
-
     }
+
 }
