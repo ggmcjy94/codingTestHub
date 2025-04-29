d85d68357808bb77620742311e85b41be747dc52
Lee Hyun SEOK <54434182+ggmcjy94@users.noreply.github.com>
2025-04-29 09:48:51 +0900

    [Gold V] Title: 적록색약, Time: 136 ms, Memory: 16588 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,103 +1,100 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
 
-    static BufferedReader br;
-    static BufferedWriter bw;
-    static int[][] arr;
+
+    static int N;
+    static char[][] map, gMap;
     static boolean[][] visited;
-    static int[] result;
-    static int[] dx = {1,0,-1,0};
-    static int[] dy = {0,1,0,-1};
-    static int N,M,r,g,b;
-    static int min_count = Integer.MAX_VALUE;
-//5
-//RRRBB
-//GGBBB
-//BBBRR
-//BBRRR
-//RRRRR
-// 적록색약이 아닌 사람이 봤을 때 구역의 수는 총 4개이다. (빨강 2, 파랑 1, 초록 1)
-// 하지만, 적록색약인 사람은 구역을 3개 볼 수 있다. (빨강-초록 2, 파랑 1)
-    static void bfs(int x, int y, char target, boolean c) {
-        Queue<int []> queue = new LinkedList<>();
-        queue.add(new int[]{x, y});
+    static int[] dx = {0 ,0 ,-1,1};
+    static int[] dy = {1, -1, 0, 0};
+
+    static void dfs(int x, int y) {
+        char a = map[x][y];
         visited[x][y] = true;
-        while (!queue.isEmpty()) {
-            int[] poll = queue.poll();
-            int now_x = poll[0];
-            int now_y = poll[1];
-            for (int i = 0; i < 4; i++) {
-                int nx = now_x + dx[i];
-                int ny = now_y + dy[i];
-                if (nx < 0 || nx >=N || ny < 0 || ny >= N) continue;
-                if (c) {
-                    if (arr[nx][ny] != 'B' && !visited[nx][ny]) {
-                        queue.add(new int[]{nx, ny});
-                        visited[nx][ny] = true;
-                    }
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx <0 || ny < 0 || nx>=N || ny >= N || visited[nx][ny]) continue;
+            if (a == map[nx][ny]) {
+                visited[nx][ny] = true;
+                dfs(nx, ny);
+            }
+        }
+
+    }
+    static void gdfs(int x, int y) {
+        char a = gMap[x][y];
+        visited[x][y] = true;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx <0 || ny < 0 || nx>=N || ny >= N || visited[nx][ny]) continue;
+            if (a == gMap[nx][ny]) {
+                visited[nx][ny] = true;
+                gdfs(nx, ny);
+            }
+
+        }
+
+    }
+
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
+        gMap = new char[N][N];
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for(int j = 0; j < N; j++) {
+                map[i][j] = str.charAt(j);
+                if (map[i][j] =='G') {
+                    gMap[i][j] = 'R';
                 } else {
-                    if (arr[nx][ny] == target && !visited[nx][ny]) {
-                        queue.add(new int[]{nx, ny});
-                        visited[nx][ny] = true;
-                    }
+                    gMap[i][j] = map[i][j];
                 }
+
 
             }
         }
-    }
-    public static void main(String[] args) throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
-        for (int i = 0; i<N; i++) {
-            String s = br.readLine();
-            for (int j = 0; j<N; j++) {
-                arr[i][j] = s.charAt(j);
-            }
-        }
-        int sum = 0;
         visited = new boolean[N][N];
-        for (int i = 0; i<N; i++) {
-            for (int j = 0; j<N; j++) {
-                if (arr[i][j] == 'R' && !visited[i][j]) {
-                    bfs(i, j,'R', false);
-                    r++;
-                }
-                if (arr[i][j] == 'G' && !visited[i][j]) {
-                    bfs(i, j,'G',false);
-                    g++;
-                }
-                if (arr[i][j] == 'B' && !visited[i][j]) {
-                    bfs(i, j,'B',false);
-                    b++;
+        int res = 0;
+        int gRes = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    visited[i][j] = true;
+                    res++;
+                    dfs(i, j);
                 }
             }
         }
-        sum += r+g+b;
-        bw.write(sum+" ");
-        sum = 0;
-        r = 0;
-        b = 0;
+
+
+
         visited = new boolean[N][N];
-        for (int i = 0; i<N; i++) {
-            for (int j = 0; j<N; j++) {
-                if (arr[i][j] != 'B' && !visited[i][j]) {
-                    bfs(i, j,'R', true);
-                    r++;
-                }
-                if (arr[i][j] == 'B' && !visited[i][j]) {
-                    bfs(i, j,'B',false);
-                    b++;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    visited[i][j] = true;
+                    gRes++;
+                    gdfs(i, j);
                 }
             }
         }
-        sum += r+b;
-        bw.write(sum+"");
+        bw.write(res+" " + gRes);
         bw.flush();
         bw.close();
+        br.close();
+
+
     }
+
+
+
+
 }
